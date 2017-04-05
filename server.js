a0334ae60ad321da90250e8bb0a7506550ff5317
Kartik Mathpal <kmathpal684@gmail.com>
2017-04-05 22:57:31 +0530

    adds /todos & todos/:id routes

--- server.js
+++ server.js
@@ -1,21 +1,62 @@
 var express = require('express');
 var app     = express(); //application instance
 var PORT    = process.env.PORT || 3000;
+var todos   = [{
+	id          : 1,
+	description : 'meet mom for lunch',
+	completed   : false
+},
+{
+	id          : 2,
+	description : 'Goto market',
+	completed   : false
+},
+{
+	id          : 3,
+	description : 'Goto dance class',
+	completed   : true
+}
+];
 
 app.get('/',function(req,res){
 	res.send('ToDo Api route....');
-}).listen(PORT,function(){
-	console.log('Server started...');
+});
+
+
+app.get('/todos',function(req,res){
+	//res.send(JSON.stringify(todos));
+	res.json(todos);
+
+});
+
+app.get('/todos/:id',function(req,res){
+	  var todoID = parseInt(req.params.id,10); //A parameter is always a string
+
+	  var matchedToDo;
+	  todos.forEach(function(todo){
+	  	if(todoID === todo.id){
+	  		matchedToDo = todo;
+	  	}
+	  });
+	  if(matchedToDo){
+	  	res.json(matchedToDo);
+	  }else{
+	  	res.status(404).send();
+	  }
+	// var limit  = todos.length;
+	// //console.log(limit);
+	// var itr    = 0;
+	// while(itr < limit){
+	// 	if(todos.itr.id === todoID){
+	// 		res.json(todos.itr);
+	// 	}
+	// 	itr = itr + 1 ;
+	// }
+	res.send(req.params.id);
 });
 
 
 
-/*
-1.heroku create : makes a new heroku app and adds a heroku git remote
-
-2.heroku rename APPNAME
-
-//make sure commit done
-3. git push heroku master
-
-*/
+app.listen(PORT,function(){
+	console.log('Server started...');
+});
