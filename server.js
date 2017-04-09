1ad22db23f514f3fbe6ebaf3bd6ae31b986665c5
Kartik Mathpal <kmathpal684@gmail.com>
2017-04-09 15:43:54 +0530

    Adds filter

--- server.js
+++ server.js
@@ -41,6 +41,11 @@ app.get('/todos',function(req,res){
 	}
 	else if(queryParams.hasOwnProperty('completed') && queryParams.completed == 'false'){
 			filteredTodos = _.where(filteredTodos,{completed:false}); 
+	} 
+	if(queryParams.hasOwnProperty('q') && queryParams.q.length > 0){
+			filteredTodos = _.filter(todos,function(todo){
+				return todo.description.toLowerCase().indexOf(queryParams.q) > -1  //<==============================
+			})
 	}
 	res.send(filteredTodos);
 
