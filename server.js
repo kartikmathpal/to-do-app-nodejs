778284666ea6fd32fd03a72fe1d05d18b0e0ec45
Kartik Mathpal <kmathpal684@gmail.com>
2017-04-16 19:34:13 +0530

    Add sequelize demo

--- server.js
+++ server.js
@@ -4,35 +4,19 @@ var bodyParser = require('body-parser');
 var PORT       = process.env.PORT || 3000;
 var _          = require('underscore');
 
-// var todos   = [{
-// 	id          : 1,
-// 	description : 'meet mom for lunch',
-// 	completed   : false
-// },
-// {
-// 	id          : 2,
-// 	description : 'Goto market',
-// 	completed   : false
-// },
-// {
-// 	id          : 3,
-// 	description : 'Goto dance class',
-// 	completed   : true
-// }
-// ];
 var todos      = [];
 var todoNextID = 1;
+//middleware
 app.use(bodyParser.json()); //process incoming JSON request
 
-//--------------------------------------------------------------routes-----
+//---------------------------------------READ--------------------------------------------------------------------------
 app.get('/',function(req,res){
 	res.send('ToDo Api route....');
 });
 
-//-------------------------------------------------------------------------
+//---------------------------------------READ--------------------------------------------------------------------------
 app.get('/todos',function(req,res){
-	//res.send(JSON.stringify(todos));
-	//res.json(todos);
+	  
 	var queryParams = req.query;
 	var filteredTodos = todos;
   //adding filter
@@ -50,25 +34,12 @@ app.get('/todos',function(req,res){
 	res.send(filteredTodos);
 
 });
-//-------------------------------------------------------------------------
+//---------------------------------------READ--------------------------------------------------------------------------
 app.get('/todos/:id',function(req,res){
-	  //var todoID = parseInt(req.params.id,10); //A parameter is always a string
-    // var matchedToDo;
-	  // todos.forEach(function(todo){
-	  // 	if(todoID === todo.id){
-	  // 		matchedToDo = todo;
-	  // 	}
-	  // });
-    //  if(matchedToDo){
-	  // 	res.json(matchedToDo);
-	  // }else{
-	  // 	res.status(404).send();
-	  // }
+	   
 	  var todoID = parseInt(req.params.id,10);
 	  var matchedToDo = _.findWhere(todos, {id :todoID});
-	   //findWhere Looks through the list and returns the first value that matches all of the key-value pairs listed in properties.
-     //If no match is found, or if list is empty, undefined will be returned.
-	 
+
 	  if(matchedToDo){
 			res.json(matchedToDo);
 		}else{
@@ -77,41 +48,32 @@ app.get('/todos/:id',function(req,res){
 		  console.log('Error 404 sent!');
 		}
 	});
-//-------------------------------------------------------------------------
+//--------------------------------------CREATE---------------------------------------------------------------------------
 app.post('/todos',function(req,res){
-	//var body = req.body; //access
+	 
 	var body = _.pick(req.body,'description','completed');
-	/*
-	 pick_.pick(object, *keys) 
-   Return a copy of the object, filtered to only have 
-   values for the whitelisted keys (or array of valid keys). Alternatively accepts a predicate indicating which keys to pick.
-  */
+	 
 	if(!_.isBoolean (body.completed) || !_.isString(body.description) || body.description.trim().length === 0 ){
 		return res.status(400).send();
 	}
 	body.description = body.description.trim();
   //add id 
-	body.id = todoNextID ;          //body.id = todoNextID++ ;
+	body.id = todoNextID ;          
   todoNextID = todoNextID + 1 ;
 
   //push into array
   todos.push(body);
-	//console.log('description :' , body.description);
 
 	res.json(body);
 
 });
-//-------------------------------------------------------------------------
+//--------------------------------------DELETE---------------------------------------------------------------------------
 
 app.delete('/todos/:id',function(req,res){
 
 	var todoID = parseInt(req.params.id,10);
   var matchedToDo = _.findWhere(todos, {id :todoID});
   if(matchedToDo){
-  	/*
-	without_.without(array, *values) 
-  Returns a copy of the array with all instances of the values removed.
-	*/
   todos = _.without(todos,matchedToDo);	
   res.send(todos);
   }
@@ -119,7 +81,7 @@ app.delete('/todos/:id',function(req,res){
   	res.status(404).json({"error" : "no to-do found with that id!!"});
   }
 });
-//-------------------------------------------------------------------------
+//------------------------------------UPDATE-----------------------------------------------------------------------------
 app.put('/todos/:id',function(req,res){
 		var todoID = parseInt(req.params.id,10);
 		var matchedToDo = _.findWhere(todos,{id : todoID});
@@ -137,23 +99,12 @@ app.put('/todos/:id',function(req,res){
 		}else if(body.hasOwnProperty('description')){
 				res.status(400).send();
 		}
-		
-		//res.send(validAttributes);
-
-		/*
-			_.extend(destination, *sources) 
-			Shallowly copy all of the properties in the source objects over to the destination object, 
-			and return the destination object. Any nested objects or arrays will be copied by reference, 
-			not duplicated. It's in-order, so the last source will override properties of the same name in previous arguments.
-
-		*/
-		//objects in javascript are passed by reference
 		 _.extend(matchedToDo,validAttributes);
 		 res.json(matchedToDo);
 
 
 });
-//-------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------------------------
 app.listen(PORT,function(){
 	console.log('Server started...');
 });
